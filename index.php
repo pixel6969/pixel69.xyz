2f75ebe333d3680f6359069a6ec5e26055b6e40e
pixel6969 <124809483+pixel6969@users.noreply.github.com>
2023-02-15 10:13:12 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+pixel
+</title>
+</head>
+<body background="/img/bg.gif" text="white" link="blue" vlink="cyan">
+<center>
+<br>
+<br>
+<img src="/img/pixel.gif">
+<font face="castoro">
+<p>ditch css and write everything in html.</p>
+<p>dont know how to? <a href="https://theoldnet.com/get?url=http%3A%2F%2Fwww.geocities.com%2FAthens%2F2090%2F&year=1996&scripts=false&decode=false">learn here!</a></p>
+</font>
+<br>
+<br>
+<br>
+<br>
+<hr color="blue">
+<font face="castoro" size="25">
+<p>image of the month!</p>
+<img src="img/black.jpg">
+</font>
+<hr color="blue">
+</center>
